b12c292da83d72425e37226208607ab97171ce10
jobenas <jobenas@energyatech.com>
2020-06-27 12:19:23 -0500

    ch2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,39 @@ class HomePage extends StatelessWidget{
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        child: Text("Hi Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red,
+            // shape: BoxShape.circle
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              colors: [
+                Colors.pink,
+                Colors.yellow
+              ]
+              ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10
+              )
+            ]
+          ),
+          child: Text(
+            "I am Box",
+            textAlign: TextAlign.center, 
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+              fontWeight: FontWeight.bold
+            )
+          ),
+        ),
       ),
     );
   }
