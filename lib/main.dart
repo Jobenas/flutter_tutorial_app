bc97d505f4bd9ad8c3823a88c3d5198eed1df711
jobenas <jobenas@energyatech.com>
2020-06-27 18:12:03 -0500

    chapter 4 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,87 @@
 import "package:flutter/material.dart";
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: HomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
-   )
-  );
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.blue),
+  ));
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var myText = "Change My Name";
+  TextEditingController _nameController = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text("Awesome App"),
       ),
-      body: Container(
-        height: 500,
-        color: Colors.teal,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.red,
-              alignment: Alignment.center,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-              alignment: Alignment.center,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
-              alignment: Alignment.center,
-            ),
-          ]
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Card(
+                child: Column(
+              children: <Widget>[
+                Image.asset("assets/coding_img1.jpg"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  myText,
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  controller: _nameController,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: "Enter Some Text",
+                      labelText: "Name"),
+                )
+              ],
+            )),
+          ),
         ),
+      ),
+      drawer: Drawer(
+          child: ListView(padding: const EdgeInsets.all(0), children: <Widget>[
+        UserAccountsDrawerHeader(
+            accountName: Text("jobenas"),
+            accountEmail: Text("jobenas@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://pbs.twimg.com/profile_images/937730650307776513/16A6uQpM_400x400.jpg"),
+            )),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("jobenas"),
+          subtitle: Text("Dev"),
+          trailing: Icon(Icons.edit),
+          onTap: () {},
+        ),
+        ListTile(
+            leading: Icon(Icons.email),
+            title: Text("Email"),
+            subtitle: Text("jobenas@gmail.com"),
+            trailing: Icon(Icons.send))
+      ])),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
       ),
     );
   }
-
 }
